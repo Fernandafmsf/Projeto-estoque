be7b8a6fbd036d48658269416ffd66b8f499011f
Fernandafmsf <fernandafernandesauto@gmail.com>
2023-09-08 10:13:48 -0300

    correcting connection

--- connection.php
+++ connection.php
@@ -1,8 +1,13 @@
 <?php
 //conectando ao DB e fazendo tratamento de erro com try catch
+$host = "localhost:3309";
+$db = "loja";
+$username = "admin";
+$password = "admin";
+
 try
 {
-  $pdo=new PDO('mysql:host=localhost;dbname=loja', 'root', 'admin');
+  $pdo=new PDO('mysql:host=localhost:3306;dbname=loja', 'admin', 'admin');
 }
 catch(PDOexception $e)
 {
